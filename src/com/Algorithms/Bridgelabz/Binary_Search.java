5263cc01f6951e1c601ba9032f8e968ec42d2c08
SnehalRFP-175 <110177238+SnehalRFP-175@users.noreply.github.com>
2022-09-22 11:16:28 +0530

    Update Binary_Search.java

--- src/com/Algorithms/Bridgelabz/Binary_Search.java
+++ src/com/Algorithms/Bridgelabz/Binary_Search.java
@@ -1,27 +1,77 @@
 package com.Algorithms.Bridgelabz;
-import java.lang.reflect.Array;
-import java.util.*;
 
-public class Binary_Search 
+import java.util.Scanner;
+
+public class Binary_Search
 {
+	int binarySearch(int element[] , int key , int start , int end)
+	{
+		
+		while(start <= end)
+		{
+			int mid = (end +start)/2;
+			
+			if(element[mid] == key)
+			{
+				return mid;
+			}
+			
+			if (element[mid] < key)
+			{
+				start = mid + 1;
+			}
+			
+			else
+				end = mid - 1;
+		}
+		
+		return 1;
+		
+}
+	
+	
 	public static void main(String a[])
 	{
 		
-		String arr[] = {"Sneha","Pranit","Prachi","Puja"};
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.println(arr);
+		System.out.println("Enter the Number of Array Element Do you Want :");
+		int num = sc.nextInt();
+		System.out.println("Enter the Array Element");
+		
+		int i;
+		int element[] = new int[num];
+		for ( i =0 ; i < num ;i++)
+		{
+			element[i] = sc.nextInt();
+			
+		}
+		System.out.println("Array Elements:");
+		for ( i =0 ; i <num ;i++)
+		{
+			System.out.print(" "+element[i]);
+				
+		}
+		int n = element.length;
+		
+		System.out.println("\n Enter the key :");
+		int key = sc.nextInt();		
+		
+		Binary_Search b  = new Binary_Search();
+		
+		int result = b.binarySearch(element, key, 0, n - 1);
 		
 		
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the key:-");
-		String key=sc.toString(); 
-	   int result=Arrays.binarySearch(arr,key);
-       if (result <0)
-       {
-    	   System.out.println("Element is not found!"); 
-       }
-       else  
-               
-    	   System.out.println("Element is found: "+result);
+		for ( i =0 ; i <num ;i++)
+		{
+			if(key == element[i])
+			{
+				System.out.println("Key is Founded at location ->" +result);
+			}
+			
+		}
+		
+				
 	}
+
 }
